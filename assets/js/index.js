8dc73ae610021805a611229252df7352eda3e9fa
rubenchu-389 <rubenchu389@gmail.com>
2024-05-19 19:27:14 -0400

    arreglos pagina

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+// $(document).scroll(function (e) {
+//     const ubicacionScroll = $("html").scrollTop();
+//     console.log("-- ubicacionScroll scroll es --> ", ubicacionScroll);
+//     if (ubicacionScroll > 1400) {
+//       console.log("Estamos por mayor a 1400");
+//       // Cuando pasamos x ubicacionScroll
+//       $("footer").removeClass("footerDarck").addClass("footerTransparent")
+//     } else {
+//       $("footer").removeClass("footerTransparent").addClass("footerDarck")
+//       console.log("footer menor a 1400"); 
+//     }
+//   });
